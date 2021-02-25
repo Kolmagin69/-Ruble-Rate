d3831b9736a6ef3df33211ffcde3135350f6737e
Kolmagin69 <kolmaginnikita@gmail.com>
2021-02-26 02:32:42 +0300

    commit

--- src/main/java/com/check/rate/ruble/client/open/change/rates/item/ExchangeRate.java
+++ src/main/java/com/check/rate/ruble/client/open/change/rates/item/ExchangeRate.java
@@ -1,16 +1,18 @@
 package com.check.rate.ruble.client.open.change.rates.item;
 
 public enum ExchangeRate {
-
-    AED, AFN, ALL, AMD, ANG, AOA, ARS, AUD, AWG, AZN, BAM, BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD,
-    BTC, BTN, BWP, BYN, BYR, BZD, CAD, CDF, CHF, CLF, CLP, CNH, CNY, COP, CRC, CUC, CUP, CVE, CZK, DJF, DKK,
-    DOP, DZD, EEK, EGP, ERN, ETB, EUR, FJD, FKP, GBP, GEL, GGP, GHS, GIP, GMD, GNF, GTQ, GYD, HKD, HNL, HRK,
-    HTG, HUF, IDR, ILS, IMP, INR, IQD, IRR, ISK, JEP, JMD, JOD, JPY, KES, KGS, KHR, KMF, KPW, KRW, KWD, KYD,
-    KZT, LAK, LBP, LKR, LRD, LSL, LYD, MAD, MDL, MGA, MKD, MMK, MNT, MOP, MRO, MRU, MTL, MUR, MVR, MWK, MXN,
-    MYR, MZN, NAD, NGN, NIO, NOK, NPR, NZD, OMR, PAB, PEN, PGK, PHP, PKR, PLN, PYG, QAR, RON, RSD, RUB, RWF,
-    SAR, SBD, SCR, SDG, SEK, SGD, SHP, SLL, SOS, SRD, SSP, STD, STN, SVC, SYP, SZL, THB, TJS, TMT, TND, TOP,
-    TRY, TTD, TWD, TZS, UAH, UGX, USD, UYU, UZS, VEF, VND, VUV, WST, XAF, XAG, XAU, XCD, XDR, XOF, XPD, XPF,
-    XPT, YER, ZAR, ZMK, ZMW, BTS, DASH, DOGE, EAC, EMC, ETH, FCT, FTC, LD , LTC, NMC, NVC, NXT , PPC, STR,
-    VTC, XMR, XPM, XRP, VEF_BLKMKT, VEF_DICOM, VEF_DIPRO
-
+    AED,    AFN,    ALL,    AMD,    ANG,    AOA,    ARS,    AUD,    AWG,    AZN,    BAM,    BBD,    BDT,    BGN,
+    BHD,    BIF,    BMD,    BND,    BOB,    BRL,    BSD,    BTC,    BTN,    BTS,    BWP,    BYN,    BZD,    CAD,
+    CDF,    CHF,    CLF,    CLP,    CNH,    CNY,    COP,    CRC,    CUC,    CUP,    CVE,    CZK,    DAS,    DJF,
+    DKK,    DOG,    DOP,    DZD,    EAC,    EGP,    EMC,    ERN,    ETB,    ETH,    EUR,    FCT,    FJD,    FKP,
+    FTC,    GBP,    GEL,    GGP,    GHS,    GIP,    GMD,    GNF,    GTQ,    GYD,    HKD,    HNL,    HRK,    HTG,
+    HUF,    IDR,    ILS,    IMP,    INR,    IQD,    IRR,    ISK,    JEP,    JMD,    JOD,    JPY,    KES,    KGS,
+    KHR,    KMF,    KPW,    KRW,    KWD,    KYD,    KZT,    LAK,    LBP,    LD,     LKR,    LRD,    LSL,    LTC,
+    LYD,    MAD,    MDL,    MGA,    MKD,    MMK,    MNT,    MOP,    MRO,    MRU,    MUR,    MVR,    MWK,    MXN,
+    MYR,    MZN,    NAD,    NGN,    NIO,    NMC,    NOK,    NPR,    NVC,    NXT,    NZD,    OMR,    PAB,    PEN,
+    PGK,    PHP,    PKR,    PLN,    PPC,    PYG,    QAR,    RON,    RSD,    RUB,    RWF,    SAR,    SBD,    SCR,
+    SDG,    SEK,    SGD,    SHP,    SLL,    SOS,    SRD,    SSP,    STD,    STN,    STR,    SVC,    SYP,    SZL,
+    THB,    TJS,    TMT,    TND,    TOP,    TRY,    TTD,    TWD,    TZS,    UAH,    UGX,    USD,    UYU,    UZS,
+    VEF,    VES,    VND,    VTC,    VUV,    WST,    XAF,    XAG,    XAU,    XCD,    XDR,    XMR,    XOF,    XPD,
+    XPF,    XPM,    XPT,    XRP,    YER,    ZAR,    ZMW,    ZWL
     }
